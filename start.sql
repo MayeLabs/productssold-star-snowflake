ecc40dfc52ee4f427521fac4764557983428efe1
MayeLabs <mayeaguilera96@gmail.com>
2025-10-31 06:05:22 -0500

    Start model: dimension table dim_customer

--- start.sql
+++ start.sql
@@ -1,11 +1,18 @@
 /*** Products sold model start ***/
 
 
--- Dimention table: dim_date
+-- Dimension table: dim_date
 CREATE TABLE dim_date (
     date_id INT PRIMARY KEY,
     date DATE,
     month VARCHAR(20),
     year INT 
-)
+);
+
+-- Dimension table: dim_customer
+CREATE TABLE dim_customer(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    location VARCHAR(100)
+);
 
